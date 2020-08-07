d13a4a148f3b43398e47ac96c59d629dd22cd695
Erick Wendel <erick.workspace@gmail.com>
2020-08-07 19:10:44 -0300

    add example03
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- example02-sls-framework/03-serverless-dynamodb/handler.js
+++ example02-sls-framework/03-serverless-dynamodb/handler.js
@@ -1,18 +1,17 @@
 'use strict';
+const AWS = require('aws-sdk')
+const DynamoDB = new AWS.DynamoDB({
+  endpoint: 'http://localhost:4566'
+})
 
 module.exports.hello = async event => {
+  const allData = await DynamoDB.scan({
+    TableName: 'Heroes',
+  }).promise()
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(allData),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
