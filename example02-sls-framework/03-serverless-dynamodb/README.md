d1c1f6e0c6c44502f6e7307dc13e87d5abbe75dc
Erick Wendel <erick.workspace@gmail.com>
2020-08-07 19:52:26 -0300

    add logs and readme
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ example02-sls-framework/03-serverless-dynamodb/README.md
@@ -0,0 +1,8 @@
+# 03-serverless-dynamodb
+
+## Running 
+
+- `docker-compose up -d`
+- `npm i`
+- `npm start`
+- `npm run curl`
---
+++ example02-sls-framework/03-serverless-dynamodb/logs.sh
@@ -0,0 +1,27 @@
+# sh utils.sh
+
+# Global
+alias awslocal="aws --endpoint-url=http://localhost:4566"
+
+# CloudWatch
+APP_NAME=serverless-dynamodb
+LOG_GROUP=$(awslocal logs describe-log-groups \
+    | \
+    jq -r '.logGroups[].logGroupName' \
+    | grep $APP_NAME
+)
+
+LOGS=$(awslocal logs describe-log-streams \
+        --log-group-name $LOG_GROUP \
+        | \
+        jq -r '.logStreams[].logStreamName'
+)
+for log in $(printf '%s\n' "${LOGS[@]}"); do
+    echo 'logging' $log
+    awslocal logs get-log-events \
+        --log-group-name $LOG_GROUP \
+        --log-stream-name $log \
+        | \
+        jq -r '.events[].message'
+done
+
